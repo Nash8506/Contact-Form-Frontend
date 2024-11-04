2146ea1695b848add6073b0c7a7d66477893f0c7
Nashid K <kp.nashid.nashid@gmail.com>
2024-11-04 21:53:31 +0530

    ContactForm.js modified

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -38,7 +38,7 @@ function ContactForm() {
     setErrors(validationErrors);
     if (Object.keys(validationErrors).length === 0) {
       try {
-        const response = await fetch("http://localhost:5000/contact", {
+        const response = await fetch("process.env.REACT_APP_API_URL", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
